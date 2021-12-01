0263901e6cdd17a2f6090f5124064fc92ffcdf3c
molgga84 <molgga84@gmail.com>
2021-12-01 22:08:27 +0900

    demo ing

--- projects/demo/src/app/pages/custom-open-strategy/custom-open-strategy.ts
+++ projects/demo/src/app/pages/custom-open-strategy/custom-open-strategy.ts
@@ -7,13 +7,17 @@ export class CustomOpenStrategy implements OpenStrategy {
     this.duration = duration;
     const timingOpacity = this.duration * 0.85;
     return {
-      modal: { transformStyle: 'preserve-3d', perspective: '1000px' },
+      modal: {
+        transition: `transform ${duration}ms ${duration}ms cubic-bezier(0.215, 0.610, 0.355, 1)`,
+        transform: 'translateY(120px) scale(0.75, 0.75)',
+      },
       pivot: {
         transition: `transform ${duration}ms cubic-bezier(0.215, 0.610, 0.355, 1), opacity ${timingOpacity}ms`,
-        transform: 'translateY(-300px) rotateX(135deg) scale(0.2, 0.2)',
+        transform: 'translateY(-300px) scale(0.2, 0.2)',
+        border: '10px solid #333',
         opacity: 0,
       },
-      overlay: { opacity: 0, backgroundColor: '#ff0' },
+      overlay: { opacity: 0, backgroundColor: '#000' },
     };
   }
 
@@ -21,7 +25,7 @@ export class CustomOpenStrategy implements OpenStrategy {
   shadow(): OpenStrategyStyleSet {
     return {
       pivot: {
-        boxShadow: '0 3px 4px rgba(0,0,0,0.3)',
+        boxShadow: 'initial',
       },
     };
   }
@@ -30,6 +34,7 @@ export class CustomOpenStrategy implements OpenStrategy {
   // start open styles
   opening(): OpenStrategyStyleSet {
     return {
+      modal: { transform: 'translateY(0px) scale(1,1)' },
       pivot: { transform: 'translateY(50px) rotateX(15deg) scale(1, 1)', opacity: 1 },
       overlay: { opacity: 0.7 },
     };
@@ -41,18 +46,21 @@ export class CustomOpenStrategy implements OpenStrategy {
     return [
       {
         // last
+        modal: { transform: 'translateY(0px) scale(1,1)' },
         pivot: { transform: 'translateY(-60px) rotateX(-10deg) scale(0.85, 0.85)', opacity: 1 },
         overlay: { opacity: 0.1 },
       },
       {
         // second
+        modal: { transform: 'translateY(0px) scale(1,1)' },
         pivot: { transform: 'translateY(-40px) rotateX(-5deg) scale(0.9, 0.9)', opacity: 1 },
-        overlay: { opacity: 0.3 },
+        overlay: { opacity: 0.1 },
       },
       {
         // first
+        modal: { transform: 'translateY(0px) scale(1,1)' },
         pivot: { transform: 'translateY(50px) rotateX(15deg) scale(1, 1)', opacity: 1 },
-        overlay: { opacity: 0.7 },
+        overlay: { opacity: 0.2 },
       },
     ];
   }
