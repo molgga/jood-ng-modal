fe0b94dab9c1881647adf5c50c29d1b2ed8c910e
이윤석 <ys84@wemakeprice.com>
2021-12-07 13:54:49 +0900

    docs(demo): assets copy 누락 수정 openStrategy demo 수정

--- projects/demo/src/app/pages/open-strategy/custom-open-strategy.ts
+++ projects/demo/src/app/pages/open-strategy/custom-open-strategy.ts
@@ -2,77 +2,71 @@ import { OpenStrategy, OpenStrategyStyleSet } from '@jood/ng-modal';
 
 export class CustomOpenStrategy implements OpenStrategy {
   duration = 240;
-  // base styles
+
   base(duration: number = 240): OpenStrategyStyleSet {
     this.duration = duration;
     const timingOpacity = this.duration * 0.85;
     return {
       modal: {
-        transition: `transform ${duration}ms ${duration}ms cubic-bezier(0.215, 0.610, 0.355, 1)`,
-        transform: 'translateY(120px) scale(0.75, 0.75)',
+        transformStyle: 'preserve-3d',
+        perspective: '1000px',
       },
       pivot: {
         transition: `transform ${duration}ms cubic-bezier(0.215, 0.610, 0.355, 1), opacity ${timingOpacity}ms`,
-        transform: 'translateY(-300px) scale(0.2, 0.2)',
-        border: '10px solid #333',
+        transform: 'translateY(-300px) rotateX(135deg) scale(0.2, 0.2)',
         opacity: 0,
       },
-      overlay: { opacity: 0, backgroundColor: '#000' },
-    };
-  }
-
-  // base shadow
-  shadow(): OpenStrategyStyleSet {
-    return {
-      pivot: {
-        boxShadow: 'initial',
+      overlay: {
+        opacity: 0,
       },
     };
   }
 
-  // option - flotingMode: false (default)
-  // start open styles
-  opening(): OpenStrategyStyleSet {
+  shadow(): OpenStrategyStyleSet {
     return {
-      modal: { transform: 'translateY(0px) scale(1,1)' },
-      pivot: { transform: 'translateY(50px) rotateX(15deg) scale(1, 1)', opacity: 1 },
-      overlay: { opacity: 0.7 },
+      pivot: {},
     };
   }
 
-  // option - flotingMode: true
-  // start open styles
+  opening(): OpenStrategyStyleSet {
+    return {
+      pivot: {
+        transform: 'translateY(50px) rotateX(15deg) scale(1, 1)',
+        opacity: 1,
+      },
+      overlay: { opacity: 0.3 },
+    };
+  }
+
   floatingOpening(): OpenStrategyStyleSet[] {
     return [
       {
         // last
-        modal: { transform: 'translateY(0px) scale(1,1)' },
         pivot: { transform: 'translateY(-60px) rotateX(-10deg) scale(0.85, 0.85)', opacity: 1 },
-        overlay: { opacity: 0.1 },
+        overlay: { opacity: 0.3, backgroundColor: 'rgba(0,0,255,1)' },
       },
       {
         // second
-        modal: { transform: 'translateY(0px) scale(1,1)' },
         pivot: { transform: 'translateY(-40px) rotateX(-5deg) scale(0.9, 0.9)', opacity: 1 },
-        overlay: { opacity: 0.1 },
+        overlay: { opacity: 0.4, backgroundColor: 'rgba(0,255,0,1)' },
       },
       {
         // first
-        modal: { transform: 'translateY(0px) scale(1,1)' },
         pivot: { transform: 'translateY(50px) rotateX(15deg) scale(1, 1)', opacity: 1 },
-        overlay: { opacity: 0.2 },
+        overlay: { opacity: 0.5, backgroundColor: 'rgba(255,0,0,1)' },
       },
     ];
   }
 
-  // opened
   opened(): OpenStrategyStyleSet {
     return {
-      pivot: { transitionTimingFunction: 'cubic-bezier(0.215, 0.610, 0.355, 1)', transform: 'translateY(0px)' },
+      pivot: {
+        transitionTimingFunction: 'cubic-bezier(0.215, 0.610, 0.355, 1)',
+        transform: 'translateY(0px)',
+      },
     };
   }
 
-  // start close
   closing(): OpenStrategyStyleSet {
     return {
       pivot: {
