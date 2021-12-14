4d9498b293e1d7a59689f3068f453e195b9e3462
이윤석 <ys84@wemakeprice.com>
2021-12-14 09:49:31 +0900

    demo: 옵션 관련 데모 추가 진행중 2

--- projects/demo/src/app/pages/material.module.ts
+++ projects/demo/src/app/pages/material.module.ts
@@ -3,7 +3,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
 import { MatListModule } from '@angular/material/list';
 import { MatRippleModule } from '@angular/material/core';
 import { LayoutModule } from '@angular/cdk/layout';
@@ -17,7 +20,10 @@ import { LayoutModule } from '@angular/cdk/layout';
     MatTabsModule,
     MatFormFieldModule,
     MatInputModule,
+    MatCheckboxModule,
+    MatRadioModule,
     MatSidenavModule,
+    MatSliderModule,
     MatListModule,
     MatRippleModule,
   ],
